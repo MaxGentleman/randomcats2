cb4247bf605f64c4f6e1d7241a058a0333ea0484
MaxGentleman <39035611+MaxGentleman@users.noreply.github.com>
2018-06-15 11:15:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ bot.on('ready', () => {
 });
 
 var cats;
-var cat = ["https://bit.ly/2KFuqUP","https://bit.ly/2Eyk0T3","https://bit.ly/2JVnh1w","https://read.bi/2IlgzVs","https://bit.ly/2xmrMP4","https://bit.ly/2G1cEI3","https://bit.ly/2KNl4Xj","https://bit.ly/2IcDCOE","https://bit.ly/2rviPjk","https://bit.ly/2rxnkJb","https://bit.ly/2rw2udp","https://bzfd.it/2ru8O60",];
+var cat = ["https://bit.ly/2KFuqUP","https://bit.ly/2Eyk0T3","https://bit.ly/2JVnh1w","https://read.bi/2IlgzVs","https://bit.ly/2xmrMP4","https://bit.ly/2G1cEI3","https://bit.ly/2KNl4Xj","https://bit.ly/2IcDCOE","https://bit.ly/2rviPjk","https://bit.ly/2rxnkJb","https://bit.ly/2rw2udp","https://bzfd.it/2ru8O60","https://bit.ly/2lcw7xD","];
 
 var pos;
 var po = ["http://gph.is/1UOqRIl","http://gph.is/1LjlEFE","http://gph.is/2kBGOXz","https://gph.is/1LKJYOO","http://gph.is/1syVkDT","http://gph.is/2xcqIOA","http://gph.is/1kADt78", "http://gph.is/19ljcLO","http://gph.is/1Wv2LH6","http://gph.is/1sCO2yQ","http://gph.is/1oMTlpa",];
 
+var cos;
+var co = ["http://gph.is/1UOqRIl","http://gph.is/1LjlEFE","http://gph.is/2kBGOXz",]
 
 bot.on('ready', () => {
   console.log(`${bot.user.tag} is now connected on Discord! Made by MaxGentleman #1337, version 0.76.9`);
@@ -27,6 +29,10 @@ bot.on('message', function (message){
 
     case ";gif" : pos = Math.floor(Math.random() * (po.length - 0) + 0);
                 message.channel.send(po[pos]);
+            break;
+		  
+    case ";communitycats" : cos = Math.floor(Math.random() * (co.length - 0) + 0);
+                message.channel.send(co[cos]);
             break;
 
     case ";laeti" : message.channel.send('Here is the cat of Laeti https://bit.ly/2JVIyrJ')
