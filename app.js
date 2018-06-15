c38e5f11e4e9d4bd531e8eabd21c0a2fc3d01631
MaxGentleman <39035611+MaxGentleman@users.noreply.github.com>
2018-06-15 11:56:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var pos;
 var po = ["http://gph.is/1UOqRIl","http://gph.is/1LjlEFE","http://gph.is/2kBGOXz","https://gph.is/1LKJYOO","http://gph.is/1syVkDT","http://gph.is/2xcqIOA","http://gph.is/1kADt78", "http://gph.is/19ljcLO","http://gph.is/1Wv2LH6","http://gph.is/1sCO2yQ","http://gph.is/1oMTlpa",];
 
 var cos;
-var co = ["http://gph.is/1UOqRIl","http://gph.is/1LjlEFE","http://gph.is/2kBGOXz"];
+var co = ["PhantomUniform0's Cat https://bit.ly/2rrp0DU","PhantomUniform0's Cat https://bit.ly/2HY2UEf","PhantomUniform0's Cat https://bit.ly/2wkNKne","PhantomUniform0's Cat https://bit.ly/2MreCFY","Ennalorac's Cat https://bit.ly/2ldoY00","Jeto_QC's Cat https://bit.ly/2HSY2v8"];
 
 bot.on('ready', () => {
   console.log(`${bot.user.tag} is now connected on Discord! Made by MaxGentleman #1337, version 0.76.9`);
