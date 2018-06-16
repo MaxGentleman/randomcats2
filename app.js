fbdbadd14eaf17eeecc0176b8171cd66d2383dd2
MaxGentleman <39035611+MaxGentleman@users.noreply.github.com>
2018-06-16 02:36:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bot = new Discord.Client();
 const prefix = ";";
 const version = "0.4.9";
 var cmdExec = 0;
+let data = JSON.parse(fs.readFileSync("./data.json", "utf8"));
 
 bot.login(process.env.TOKEN);
 
