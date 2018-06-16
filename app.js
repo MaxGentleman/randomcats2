837537762a818a9f02aefe6169c0d4874e4358cf
MaxGentleman <39035611+MaxGentleman@users.noreply.github.com>
2018-06-16 01:37:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 bot.login(process.env.TOKEN);
 
 bot.on('ready', () => {
-  bot.user.setGame('[;help] meowing');
+  bot.user.setActivity(prefix + "help", { type: 'PLAYING' });
 });
 
 var cats;
